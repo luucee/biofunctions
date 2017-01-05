a1dcfc8ced53f8796ef719d4ced6a6688d869be9
luigi cerulo <luigi.cerulo@gmail.com>
2017-01-05 14:25:38 +0100

    updtae

---
+++ mra-analysis.R
@@ -0,0 +1,24 @@
+
+
+activity = function(mexp,cormat,tflist=NULL,tau=0.6) {
+  mexp.s = scale(mexp)
+  if (is.null(tflist)) {
+    tflist=rownames(cormat)
+  }
+  actmat = mexp[tflist,]
+  actmat[1:length(actmat)]=0
+  for(tfi in tflist) {
+    postrg = names(cormat[tfi,cormat[tfi,] > tau])
+    negtrg = names(cormat[tfi,cormat[tfi,] < -tau])
+    apos = 1
+    if (length(postrg)>0) {
+      apos = apply(mexp.s[postrg,,drop=F],2,sum)/length(postrg)
+    }
+    aneg = 1
+    if (length(negtrg)>0) {
+      aneg = apply(mexp.s[negtrg,,drop=F],2,sum)/length(negtrg)
+    }
+    actmat[tfi,] =  apos - aneg
+  }
+  return(actmat)
+}

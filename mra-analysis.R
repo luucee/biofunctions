e7682a748e7e1d162cccfd85c720d20b3eff95fa
luigi cerulo <luigi.cerulo@gmail.com>
2017-01-05 14:26:58 +0100

    adde progress bar activity

--- mra-analysis.R
+++ mra-analysis.R
@@ -1,12 +1,14 @@
 
 
 activity = function(mexp,cormat,tflist=NULL,tau=0.6) {
+  
   mexp.s = scale(mexp)
   if (is.null(tflist)) {
     tflist=rownames(cormat)
   }
   actmat = mexp[tflist,]
   actmat[1:length(actmat)]=0
+  pb = txtProgressBar(min=1,max=length(tflist),style=3)
   for(tfi in tflist) {
     postrg = names(cormat[tfi,cormat[tfi,] > tau])
     negtrg = names(cormat[tfi,cormat[tfi,] < -tau])
@@ -19,6 +21,7 @@ activity = function(mexp,cormat,tflist=NULL,tau=0.6) {
       aneg = apply(mexp.s[negtrg,,drop=F],2,sum)/length(negtrg)
     }
     actmat[tfi,] =  apos - aneg
+    setTxtProgressBar(pb, i)
   }
   return(actmat)
 }

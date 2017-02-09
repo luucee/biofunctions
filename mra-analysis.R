e70c8f3414087b7c45a84ad46162aa0aef95d311
luigi cerulo <luigi.cerulo@gmail.com>
2017-02-09 10:18:12 +0100

    addes ssMRA

--- mra-analysis.R
+++ mra-analysis.R
@@ -28,3 +28,54 @@ activity = function(mexp,cormat,tflist=NULL,tau=0.6) {
   }
   return(actmat)
 }
+
+
+
+ssMRA = function(mexp, regulons,nperm=10000,ncore="all"){
+  require(fgsea)
+  require(doParallel)
+  if(ncore=="all"){
+    ncore = parallel:::detectCores()
+    doParallel:::registerDoParallel(cores=ncore)
+  } else{
+    doParallel:::registerDoParallel(cores=ncore)
+  }
+
+  mexp.s = mexp
+  #mexp.s = scale(mexp)
+  for(i in 1:nrow(mexp.s)){
+    mexp.s[i,] = (mexp.s[i,] - mean(mexp[i,]))/sd(mexp.s[i,])
+  }
+  
+  m<- max(unlist(lapply(regulons,length)))
+  act<-foreach (i=1:ncol(mexp.s), .combine=cbind) %dopar% 
+  {
+    s<- mexp.s[,i]
+    pheno<-sort(s,decreasing=T)
+    fgseaRes = fgsea(regulons, pheno, minSize=15, 
+                     maxSize=m, nperm=nperm)
+    list(fgseaRes) 
+    
+  }
+  ESm<- matrix(NA,nrow=length(regulons),ncol=ncol(mexp.s))
+  rownames(ESm)<- names(regulons)
+  for(i in 1:length(act)){    
+    ESm[act[[i]]$pathway ,i] <- act[[i]]$ES
+  }
+  NESm<- matrix(NA,nrow=length(regulons),ncol=ncol(mexp.s))
+  rownames(NESm)<- names(regulons)
+  for(i in 1:length(act)){    
+    NESm[act[[i]]$pathway ,i] <- act[[i]]$NES
+  }
+  PVm<- matrix(NA,nrow=length(regulons),ncol=ncol(mexp.s))
+  rownames(PVm)<- names(regulons)
+  for(i in 1:length(act)){    
+    PVm[act[[i]]$pathway ,i] <- act[[i]]$pval
+  }
+  colnames(ESm)<-colnames(mexp) 
+  colnames(NESm)<-colnames(mexp) 
+  colnames(PVm)<-colnames(mexp) 
+  ret<-list(ESm,NESm,PVm) 
+  names(ret) <- c("ES","NES","PV") 
+  return(ret)
+}

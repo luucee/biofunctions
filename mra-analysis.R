500239e20fe63703d57ea7ab95247582e6c2c75d
luigi cerulo <luigi.cerulo@gmail.com>
2017-02-09 11:31:05 +0100

    fix activity

--- mra-analysis.R
+++ mra-analysis.R
@@ -1,6 +1,6 @@
 
 
-activity = function(mexp,cormat,tflist=NULL,tau=0.6) {
+activity = function(mexp,posReg,negReg,tau=0.6) {
   
   
   mexp.s = mexp
@@ -8,20 +8,18 @@ activity = function(mexp,cormat,tflist=NULL,tau=0.6) {
   for(i in 1:nrow(mexp.s)){
     mexp.s[i,] = (mexp.s[i,] - mean(mexp[i,]))/sd(mexp.s[i,])
   }
-  if (is.null(tflist)) {
-    tflist=rownames(cormat)
-  }
+  tflist = union(names(posReg),names(negReg))
   actmat = mexp[tflist,]
   actmat[1:length(actmat)]=0
   pb = txtProgressBar(min=1,max=length(tflist),style=3)
   i=1
   for(tfi in tflist) {
-    postrg = names(cormat[tfi,cormat[tfi,] > tau])
-    negtrg = names(cormat[tfi,cormat[tfi,] < -tau])
+    postrg = posReg[[tfi]]
     apos = 1
     if (length(postrg)>0) {
       apos = apply(mexp.s[postrg,,drop=F],2,sum)/length(postrg)
     }
+    negtrg = negReg[[tfi]]
     aneg = 1
     if (length(negtrg)>0) {
       aneg = apply(mexp.s[negtrg,,drop=F],2,sum)/length(negtrg)
